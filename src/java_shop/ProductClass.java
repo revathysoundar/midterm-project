7e2bc23164987108198cf1cd62c24466385d0e5b
revathysoundar <revathy.soudar@gmail.com>
2016-09-15 15:02:28 -0400

    Initial Stage

---
+++ src/java_shop/ProductClass.java
@@ -0,0 +1,55 @@
+package java_shop;
+
+public class ProductClass {
+	
+	public String productName;
+	public String productDescription;
+	public String productCategory;
+	public String price;
+	public double grandtotal;
+	public double subTotal;
+	public String formOfPayment;
+	
+	
+	/**
+	 * Calculate SubTotal, GrandTotal, SalesTax
+	 */
+	public void calculateTotal()
+	{
+		
+	}
+	
+	/**
+	 * 
+	 * Format to 2 decimal places
+	 * @param total
+	 * @return
+	 */
+	public String formatTotal(double total)
+	{
+		return null;
+	}
+	
+	
+	/**
+	 * Mode of payment, to pay the receipt
+	 * @param formOfPayment
+	 */
+	public void paymentType(String formOfPayment)
+	{
+		
+	}
+	
+	
+	/**
+	 * Display the receipt to the User
+	 */
+	public void displayReceipt()
+	{
+		
+	}
+	
+	
+	
+
+}
